69b0d24575f956c5a43058a7d4090495c2409cec
Runbin Shi <runbin.shi@inf.ethz.ch>
2023-02-23 12:14:03 +0100

    Update tpccexec

--- HumongousLock/src/hdb/communication/DataLayer.cpp
+++ HumongousLock/src/hdb/communication/DataLayer.cpp
@@ -21,212 +21,242 @@
 #define STOCK_LEVEL_SIZE (482)
 #define YCSB_RECORD_SIZE (1032)
 
+#define NORD_WAREHOUSE_SIZE_RD (64)
+#define NORD_CUSTOMER_SIZE_RD (64)
+#define NORD_DISTRICT_WR (64)
+
+#define NORD_ITEM_RD (128)
+#define NORD_STOCK_RDWR (64)
+#define NORD_ORDLINE_WR (128)
+
+#define PAYM_WAREHOUSE_SIZE_RD (64)
+#define PAYM_DISTRICT_SIZE_RDWR (64)
+#define PAYM_CUSTOMER_SIZE_RDWR (64)
+#define PAYM_HISTORY_SIZE_WR (128)
+
+
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 
 namespace hdb {
 namespace communication {
 
 DataLayer::DataLayer(hdb::configuration::SystemConfig* config): comdelay(config->comdelay), warehouseToLockServer(config->warehouseToLockServer) {
- 
 
-	this->numberOfLockServers = config->globalNumberOfLockTableAgents;
-	this->lockServerGlobalRanks = config->lockServerGlobalRanks;
 
-	this->numberOfProcessesPerMachine = config->localNumberOfProcesses;
-	this->localMachineId = config->globalRank / numberOfProcessesPerMachine;
+  this->numberOfLockServers = config->globalNumberOfLockTableAgents;
+  this->lockServerGlobalRanks = config->lockServerGlobalRanks;
 
- 
-	if (config->isLockTableAgent) {
+  this->numberOfProcessesPerMachine = config->localNumberOfProcesses;
+  this->localMachineId = config->globalRank / numberOfProcessesPerMachine;
+
+
+  if (config->isLockTableAgent) {
 #ifdef USE_FOMPI
-		foMPI_Win_allocate(TPCC_BUFFER_SIZE, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &(buffer), &(window));
+    foMPI_Win_allocate(TPCC_BUFFER_SIZE, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &(buffer), &(window));
 #else
-		MPI_Win_allocate(TPCC_BUFFER_SIZE, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &(buffer), &(window));	
+    MPI_Win_allocate(TPCC_BUFFER_SIZE, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &(buffer), &(window));
 #endif
-		writeBuffer = NULL;
-		readBuffer = NULL;
-	} else {
+    writeBuffer = NULL;
+    readBuffer = NULL;
+  } else {
 #ifdef USE_FOMPI
-		foMPI_Win_allocate(0, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &(buffer), &(window));
+    foMPI_Win_allocate(0, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &(buffer), &(window));
 #else
-		MPI_Win_allocate(0, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &(buffer), &(window));
-#endif	
-		writeBuffer = malloc(TPCC_BUFFER_SIZE);
-		readBuffer = malloc(TPCC_BUFFER_SIZE);
-	}
+    MPI_Win_allocate(0, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &(buffer), &(window));
+#endif
+    writeBuffer = malloc(TPCC_BUFFER_SIZE);
+    readBuffer = malloc(TPCC_BUFFER_SIZE);
+  }
 
 #ifdef USE_FOMPI
-	foMPI_Win_lock_all(0, window);
+  foMPI_Win_lock_all(0, window);
 #else
-	MPI_Win_lock_all(0, window);
+  MPI_Win_lock_all(0, window);
 #endif
 
-	MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
 
 }
 
 DataLayer::~DataLayer() {
-	if (readBuffer != NULL & writeBuffer != NULL) {
-		free(readBuffer);
-		free(writeBuffer);
-	}
+  if (readBuffer != NULL & writeBuffer != NULL) {
+    free(readBuffer);
+    free(writeBuffer);
+  }
 #ifdef USE_FOMPI
-	foMPI_Win_unlock_all(window);
-	foMPI_Win_free(&(window));
+  foMPI_Win_unlock_all(window);
+  foMPI_Win_free(&(window));
 #else
-	MPI_Win_unlock_all(window);
-	MPI_Win_free(&(window));
+  MPI_Win_unlock_all(window);
+  MPI_Win_free(&(window));
 #endif
 
 }
 
 void DataLayer::newOrder(uint32_t warehouse, bool remoteItemPresent, std::set<uint32_t> *involvedLockServers) {
 
-	uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
+  uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
 
-	uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
-	if(warehouseSharedBy > 1){
-		targetLockServerInternalRank += rand() % warehouseSharedBy;
-	}
- 
- 
-	uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
-	writeToProcess(targetLockServerGlobalRank, ORDER_SIZE+NEW_ORDER_SIZE); // Put order and new order
-	if (!remoteItemPresent) {
-		writeToProcess(targetLockServerGlobalRank, LINE_ITEM_SIZE * 10); // Put 10 order lines
-	} else {
-		uint32_t remoteElements = involvedLockServers->size();
-		uint32_t localElements = (10 >= remoteElements) ? 10 - remoteElements :  0;
-		writeToProcess(targetLockServerGlobalRank, LINE_ITEM_SIZE * localElements); // Put local order lines
-		for (auto it = involvedLockServers->begin(); it != involvedLockServers->end(); ++it) {
-			uint32_t lockServerId = (*it);
-			if(lockServerId != targetLockServerGlobalRank) {
-				writeToProcess(lockServerId, LINE_ITEM_SIZE); // Put remote order lines
-			}
-		}
-	}
+  uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
+  if(warehouseSharedBy > 1){
+    targetLockServerInternalRank += rand() % warehouseSharedBy;
+  }
 
+  uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
+
+  readFromProcess(targetLockServerGlobalRank, NORD_WAREHOUSE_SIZE_RD + NORD_CUSTOMER_SIZE_RD); // Get warehouse and customer info
+  writeToProcess(targetLockServerGlobalRank, NORD_DISTRICT_WR); // Put district info
+  writeToProcess(targetLockServerGlobalRank, ORDER_SIZE+NEW_ORDER_SIZE); // Put order and new order
+
+  if (!remoteItemPresent) {
+    readFromProcess(targetLockServerGlobalRank, NORD_ITEM_RD * 10);
+    readFromProcess(targetLockServerGlobalRank, NORD_STOCK_RDWR * 10);
+    writeToProcess(targetLockServerGlobalRank, NORD_STOCK_RDWR * 10);
+    writeToProcess(targetLockServerGlobalRank, NORD_ORDLINE_WR * 10);
+
+  } else {
+    uint32_t remoteElements = involvedLockServers->size();
+    uint32_t localElements = (10 >= remoteElements) ? 10 - remoteElements :  0;
+    readFromProcess(targetLockServerGlobalRank, NORD_ITEM_RD * localElements);
+    readFromProcess(targetLockServerGlobalRank, NORD_STOCK_RDWR * localElements);
+    writeToProcess(targetLockServerGlobalRank, NORD_STOCK_RDWR * localElements);
+    writeToProcess(targetLockServerGlobalRank, NORD_ORDLINE_WR * localElements);
+    for (auto it = involvedLockServers->begin(); it != involvedLockServers->end(); ++it) {
+      uint32_t lockServerId = (*it);
+      if(lockServerId != targetLockServerGlobalRank) {
+        readFromProcess(lockServerId, NORD_ITEM_RD);
+        readFromProcess(lockServerId, NORD_STOCK_RDWR);
+        writeToProcess(lockServerId, NORD_STOCK_RDWR);
+        writeToProcess(lockServerId, NORD_ORDLINE_WR);
+      }
+    }
+  }
 }
 
 void DataLayer::payment(uint32_t warehouse) {
 
-	uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
+  uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
 
-	uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
-	if(warehouseSharedBy > 1){
-		targetLockServerInternalRank += rand() % warehouseSharedBy;
-	}
-	uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
-	readFromProcess(targetLockServerGlobalRank, CUSTOMER_SIZE); // Select customer
-	writeToProcess(targetLockServerGlobalRank, PAYMENT_DATA_SIZE); // Update payment data
+  uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
+  if(warehouseSharedBy > 1){
+    targetLockServerInternalRank += rand() % warehouseSharedBy;
+  }
+  uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
 
+  readFromProcess(targetLockServerGlobalRank, PAYM_WAREHOUSE_SIZE_RD);
+  readFromProcess(targetLockServerGlobalRank, PAYM_DISTRICT_SIZE_RDWR);
+  writeToProcess(targetLockServerGlobalRank, PAYM_DISTRICT_SIZE_RDWR);
+  readFromProcess(targetLockServerGlobalRank, PAYM_CUSTOMER_SIZE_RDWR);
+  writeToProcess(targetLockServerGlobalRank, PAYM_CUSTOMER_SIZE_RDWR);
+  writeToProcess(targetLockServerGlobalRank, PAYM_HISTORY_SIZE_WR);
 }
 
 void DataLayer::orderStat(uint32_t warehouse) {
 
-	uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
+  uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
 
-	uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
-	if(warehouseSharedBy > 1){
-		targetLockServerInternalRank += rand() % warehouseSharedBy;
-	}
-	uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
-	readFromProcess(targetLockServerGlobalRank, CUSTOMER_SIZE); // Select customer
-	bool remoteElementPresent = rand() % 10;
-	if(!remoteElementPresent) {
-		readFromProcess(targetLockServerGlobalRank, LINE_ITEM_SIZE*10);
-	} else {
-		readFromProcess(targetLockServerGlobalRank, LINE_ITEM_SIZE*9);
-		uint32_t remoteLockServer = lockServerGlobalRanks[rand() % numberOfLockServers];
-		readFromProcess(remoteLockServer, LINE_ITEM_SIZE);
-	}
+  uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
+  if(warehouseSharedBy > 1){
+    targetLockServerInternalRank += rand() % warehouseSharedBy;
+  }
+  uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
+  readFromProcess(targetLockServerGlobalRank, CUSTOMER_SIZE); // Select customer
+  bool remoteElementPresent = rand() % 10;
+  if(!remoteElementPresent) {
+    readFromProcess(targetLockServerGlobalRank, LINE_ITEM_SIZE*10);
+  } else {
+    readFromProcess(targetLockServerGlobalRank, LINE_ITEM_SIZE*9);
+    uint32_t remoteLockServer = lockServerGlobalRanks[rand() % numberOfLockServers];
+    readFromProcess(remoteLockServer, LINE_ITEM_SIZE);
+  }
 
 }
 
 void DataLayer::delivery(uint32_t warehouse) {
 
-	uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
+  uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
 
-	uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
-	if(warehouseSharedBy > 1){
-		targetLockServerInternalRank += rand() % warehouseSharedBy;
-	}
-	uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
-	readFromProcess(targetLockServerGlobalRank, ORDER_SIZE); // Get order
-	writeToProcess(targetLockServerGlobalRank, DELIVERY_DATA_SIZE); // Update delivery count and delete new-order
+  uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
+  if(warehouseSharedBy > 1){
+    targetLockServerInternalRank += rand() % warehouseSharedBy;
+  }
+  uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
+  readFromProcess(targetLockServerGlobalRank, ORDER_SIZE); // Get order
+  writeToProcess(targetLockServerGlobalRank, DELIVERY_DATA_SIZE); // Update delivery count and delete new-order
 
 }
 
 void DataLayer::slev(uint32_t warehouse) {
 
-	uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
+  uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
 
-	uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
-	if(warehouseSharedBy > 1){
-		targetLockServerInternalRank += rand() % warehouseSharedBy;
-	}
-	uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
-	for(uint32_t i=0; i<20; ++i) {
-		bool remoteElementPresent = rand() % 10;
-		if(!remoteElementPresent) {
-			readFromProcess(targetLockServerGlobalRank, 10*(LINE_ITEM_SIZE + STOCK_LEVEL_SIZE));
-		} else {
-			readFromProcess(targetLockServerGlobalRank, 9*(LINE_ITEM_SIZE + STOCK_LEVEL_SIZE));
-			uint32_t remoteLockServer = lockServerGlobalRanks[rand() % numberOfLockServers];
-			readFromProcess(remoteLockServer, LINE_ITEM_SIZE + STOCK_LEVEL_SIZE);
-		}
-	}
+  uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
+  if(warehouseSharedBy > 1){
+    targetLockServerInternalRank += rand() % warehouseSharedBy;
+  }
+  uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
+  for(uint32_t i=0; i<20; ++i) {
+    bool remoteElementPresent = rand() % 10;
+    if(!remoteElementPresent) {
+      readFromProcess(targetLockServerGlobalRank, 10*(LINE_ITEM_SIZE + STOCK_LEVEL_SIZE));
+    } else {
+      readFromProcess(targetLockServerGlobalRank, 9*(LINE_ITEM_SIZE + STOCK_LEVEL_SIZE));
+      uint32_t remoteLockServer = lockServerGlobalRanks[rand() % numberOfLockServers];
+      readFromProcess(remoteLockServer, LINE_ITEM_SIZE + STOCK_LEVEL_SIZE);
+    }
+  }
 
 }
 
 void DataLayer::ycsb(uint32_t warehouse) {
 
-	uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
+  uint32_t targetLockServerInternalRank =  warehouseToLockServer[warehouse];
 
-	uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
-	if(warehouseSharedBy > 1){
-		targetLockServerInternalRank += rand() % warehouseSharedBy;
-	}
+  uint32_t warehouseSharedBy =  warehouseToLockServer[warehouse + 1] - warehouseToLockServer[warehouse];
+  if(warehouseSharedBy > 1){
+    targetLockServerInternalRank += rand() % warehouseSharedBy;
+  }
 
-	uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
-	if(rand() % 2 == 0){
-		readFromProcess(targetLockServerGlobalRank, YCSB_RECORD_SIZE);
-	} else {
-		writeToProcess(targetLockServerGlobalRank, YCSB_RECORD_SIZE);
-	}
+  uint32_t targetLockServerGlobalRank = lockServerGlobalRanks[targetLockServerInternalRank];
+  if(rand() % 2 == 0){
+    readFromProcess(targetLockServerGlobalRank, YCSB_RECORD_SIZE);
+  } else {
+    writeToProcess(targetLockServerGlobalRank, YCSB_RECORD_SIZE);
+  }
 }
 
 void DataLayer::readFromProcess(uint32_t targetProcess, uint32_t numberOfBytes) {
 
-	bool islocal = targetProcess/numberOfProcessesPerMachine == localMachineId;
-	if(!islocal && this->comdelay != 0){
-		    auto const sleep_end_time = std::chrono::high_resolution_clock::now() +
+  bool islocal = targetProcess/numberOfProcessesPerMachine == localMachineId;
+  if(!islocal && this->comdelay != 0){
+    auto const sleep_end_time = std::chrono::high_resolution_clock::now() +
                                 std::chrono::nanoseconds(this->comdelay);
     while (std::chrono::high_resolution_clock::now() < sleep_end_time);
-	}
+  }
 
 #ifdef USE_FOMPI
-	foMPI_Get(readBuffer, numberOfBytes, MPI_BYTE, targetProcess, 0, numberOfBytes, MPI_BYTE, window);
-	foMPI_Win_flush(targetProcess, window);
+  foMPI_Get(readBuffer, numberOfBytes, MPI_BYTE, targetProcess, 0, numberOfBytes, MPI_BYTE, window);
+  foMPI_Win_flush(targetProcess, window);
 #else
-	MPI_Get(readBuffer, numberOfBytes, MPI_BYTE, targetProcess, 0, numberOfBytes, MPI_BYTE, window);
-	MPI_Win_flush(targetProcess, window);
+  MPI_Get(readBuffer, numberOfBytes, MPI_BYTE, targetProcess, 0, numberOfBytes, MPI_BYTE, window);
+  MPI_Win_flush(targetProcess, window);
 #endif
 }
 
 void DataLayer::writeToProcess(uint32_t targetProcess, uint32_t numberOfBytes) {
-	
-	bool islocal = targetProcess/numberOfProcessesPerMachine == localMachineId;
-	if(!islocal && this->comdelay != 0){
-		    auto const sleep_end_time = std::chrono::high_resolution_clock::now() +
+
+  bool islocal = targetProcess/numberOfProcessesPerMachine == localMachineId;
+  if(!islocal && this->comdelay != 0){
+    auto const sleep_end_time = std::chrono::high_resolution_clock::now() +
                                 std::chrono::nanoseconds(this->comdelay);
     while (std::chrono::high_resolution_clock::now() < sleep_end_time);
-	}
+  }
 #ifdef USE_FOMPI
-	foMPI_Put(writeBuffer, numberOfBytes, MPI_BYTE, targetProcess, 0, numberOfBytes, MPI_BYTE, window);
-	foMPI_Win_flush_local(targetProcess, window);
+  foMPI_Put(writeBuffer, numberOfBytes, MPI_BYTE, targetProcess, 0, numberOfBytes, MPI_BYTE, window);
+  foMPI_Win_flush_local(targetProcess, window);
 #else
-	MPI_Put(writeBuffer, numberOfBytes, MPI_BYTE, targetProcess, 0, numberOfBytes, MPI_BYTE, window);
-	MPI_Win_flush_local(targetProcess, window);
+  MPI_Put(writeBuffer, numberOfBytes, MPI_BYTE, targetProcess, 0, numberOfBytes, MPI_BYTE, window);
+  MPI_Win_flush_local(targetProcess, window);
 #endif
 }
 
